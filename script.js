1cd0f2d7bee52f5c7728c6fc43f65a84679b3f80
nizioleque <nizioleque@gmail.com>
2022-05-13 17:56:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const wordElement = document.querySelector('#word');
+const inputElement = document.querySelector('#input');
+const messageElement = document.querySelector('#message');
+let currentWord = '';
+
+prepareWord();
+
+function prepareWord() {
+    const index = randomIntFromInterval(0, words.length - 1);
+    const word = words[index];
+    currentWord = word;
+    wordElement.innerText = word;
+    inputElement.value = '';
+    document.body.style.backgroundColor = 'black';
+}
+
+function textInput() {
+    const currentInput = inputElement.value;
+    const substring = currentWord.substring(0, currentInput.length);
+
+    if (currentInput == substring) {
+        if (currentInput.length == currentWord.length) {
+            correctWord();
+        }
+        else {
+            document.body.style.backgroundColor = greenColor(currentInput.length, currentWord.length);
+            messageElement.style.opacity = 0;
+        }
+    }
+    else {
+        wrongWord();
+    }
+}
+
+function correctWord() {
+    messageElement.innerText = 'Dobrze!';
+    messageElement.style.opacity = 1;
+
+
+    setTimeout(() => {
+        messageElement.style.opacity = 0;
+        prepareWord();
+    }, 300);
+}
+
+function wrongWord() {
+    document.body.style.backgroundColor = '#d23232';
+    messageElement.innerText = 'Źle!';
+    messageElement.style.opacity = 1;
+}
+
+function greenColor(current, final) {
+    let lightness = current * 36 / final;
+    return 'hsl(120, 70%, ' + lightness + '%)';
+}
